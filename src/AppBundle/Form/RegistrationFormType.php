2e04795e5e9d57ed9b8f2a8e30c698d79e17f99e
Gustavo <chiappegus@gmail.com>
2018-10-10 01:24:06 -0300

    continuacion del Form

---
+++ src/AppBundle/Form/RegistrationFormType.php
@@ -0,0 +1,19 @@
+<?php
+namespace AppBundle\Form;
+
+use FOS\UserBundle\Form\Type\RegistrationFormType as BaseRegistrationFormType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstName');
+    }
+    public function getParent()
+    {
+        return BaseRegistrationFormType::class;
+    }
+}

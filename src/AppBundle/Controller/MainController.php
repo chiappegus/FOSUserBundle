a696d7753b9f4bcb8b317ec02e05587c30e2e490
Gustavo <chiappegus@gmail.com>
2018-10-12 16:37:28 -0300

    08 event no sale!!

--- src/AppBundle/Controller/MainController.php
+++ src/AppBundle/Controller/MainController.php
@@ -5,7 +5,7 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class MainController extends Controller
-{
+{   
     public function homepageAction()
     {
         return $this->render('main/homepage.html.twig');

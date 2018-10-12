624d69ee174a12d695e5dc13ea5086c018e39992
Gustavo <chiappegus@gmail.com>
2018-10-12 19:08:31 -0300

    08 event solucionado :)

--- src/AppBundle/EventListener/RedirectAfterRegistrationSubscriber.php
+++ src/AppBundle/EventListener/RedirectAfterRegistrationSubscriber.php
@@ -1,17 +1,12 @@
-<?php 
+<?php
 
-
-namespace  AppBundle\EventListener;
-
+namespace AppBundle\EventListener;
 
 use FOS\UserBundle\Event\FormEvent;
 use FOS\UserBundle\FOSUserEvents;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\RouterInterface;
-
-
-
 
 class RedirectAfterRegistrationSubscriber implements EventSubscriberInterface
 {
@@ -22,15 +17,15 @@ class RedirectAfterRegistrationSubscriber implements EventSubscriberInterface
     }
     public function onRegistrationSuccess(FormEvent $event)
     {
-        $url = $this->router->generate('homepage');
+        $url      = $this->router->generate('homepage');
         $response = new RedirectResponse($url);
         $event->setResponse($response);
     }
     public static function getSubscribedEvents()
     {
         return [
-        
-            FOSUserEvents::REGISTRATION_COMPLETED => 'onRegistrationSuccess'
+
+            FOSUserEvents::REGISTRATION_SUCCESS => 'onRegistrationSuccess',
         ];
     }
 }

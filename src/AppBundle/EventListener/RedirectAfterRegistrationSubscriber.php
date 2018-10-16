c5e38f9755099f710cacdbbeec3714a66b273f92
Gustavo <chiappegus@gmail.com>
2018-10-16 11:40:09 -0300

    09 TargetPathTrait

--- src/AppBundle/EventListener/RedirectAfterRegistrationSubscriber.php
+++ src/AppBundle/EventListener/RedirectAfterRegistrationSubscriber.php
@@ -7,17 +7,32 @@ use FOS\UserBundle\FOSUserEvents;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
+/*use Symfony\Component\Security\Http\Util\getTargetPath;*/
 
 class RedirectAfterRegistrationSubscriber implements EventSubscriberInterface
 {
+    
+    use TargetPathTrait;
+
     private $router;
+
+
     public function __construct(RouterInterface $router)
     {
         $this->router = $router;
     }
     public function onRegistrationSuccess(FormEvent $event)
-    {
-        $url      = $this->router->generate('homepage');
+    {  
+
+        #provider 
+        $url = $this->getTargetPath($event->getRequest()->getSession(), 'main');
+       
+        if (!$url) {
+
+            $url = $this->router->generate('homepage');
+        }
+        
         $response = new RedirectResponse($url);
         $event->setResponse($response);
     }
